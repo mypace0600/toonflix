2806c03dd8a74f5c4e8d8206ad0148943f22f09f
mypace0600 <mypace0600@gmail.com>
2023-05-27 23:26:44 +0900

    changed

--- lib/widgets/episode_widget.dart
+++ lib/widgets/episode_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EpisodeWidget extends StatelessWidget {
   final String id, title, rating, date;
@@ -14,23 +15,35 @@ class EpisodeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 20,
-            ),
+      onTap: () {
+        Uri url = Uri.parse("https://comic.naver.com/webtoon/");
+        launchUrl(url);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                '$rating / $date',
+                style: const TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ],
           ),
-          Text(
-            '$rating / $date',
-            style: const TextStyle(
-              fontSize: 15,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
